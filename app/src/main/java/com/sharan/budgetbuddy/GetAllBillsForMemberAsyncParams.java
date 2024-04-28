90e9af7f8ef07a33fd7ed496e0754cb59fe3d6ae
Sharan PY <sharankumar2693@gmail.com>
2024-04-28 23:31:44 +0530

    initial commit

---
+++ app/src/main/java/com/sharan/budgetbuddy/GetAllBillsForMemberAsyncParams.java
@@ -0,0 +1,12 @@
+package com.sharan.budgetbuddy;
+
+class GetAllBillsForMemberAsyncParams {
+
+    String gName;
+    int mid;
+
+    GetAllBillsForMemberAsyncParams(String gName, int mid) {
+        this.gName = gName;
+        this.mid = mid;
+    }
+}
